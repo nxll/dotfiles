9a9cce3c4a5b19ab55bd25ddc172389aef007933
nxll <nxll@teknik.io>
2016-05-29 06:01:07 +0000

    add

---
+++ bin/machine.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Message to machine
+clear
+echo "Machine,"
+echo ""
+sleep 2s
+echo "Pls make website,"
+echo ""
+sleep 2s
+echo "all responsive like,"
+sleep 2s
+echo "w/ BIG pictures ooo,"
+sleep 2s
+echo "use my fav fonts,"
+sleep 2s
+echo "also fancy menus with whooosh on"
+sleep 2s
+echo "load fast pls"
+echo ""
+sleep 2s
+echo "Thanks,"
+sleep 1s
+echo "Human"
+echo ""
+sleep 3s
+echo "PS no bugs :)"
+echo ""

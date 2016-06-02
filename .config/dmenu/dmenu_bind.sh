29826636f0d6b76203e5bf7c5ccbbbe1f23b5f0d
nxll <nxll@teknik.io>
2016-06-02 00:17:49 +0000

    add

--- .config/dmenu/dmenu_bind.sh
+++ .config/dmenu/dmenu_bind.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dmenu_run -l 8 -fn "-benis-lemon-abunai-abunai-medium-r-normal--10-110-75-75-m-50-iso10646-1" -nb "#151515" -nf "#FFFFFF" -sb "#202020" -sf "#AADB0F" -w "1920" -x "0" -y "0" &
+dmenu_run -l 10 -fn "-benis-lemon-abunai-abunai-medium-r-normal--10-110-75-75-m-50-iso10646-1" -nb "#151515" -nf "#ffffff" -sb "#aadb0f" -sf "#151515" -w "1920" -x "0" -y "0" &

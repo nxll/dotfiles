2fcecbaa7e75cdfaccad42d3820c939e04da6f25
nxll <nxll@teknik.io>
2016-11-27 00:25:44 +0000

    dynamic-colors

---
+++ .dynamic-colors/colorschemes/h4x0r.sh
@@ -0,0 +1,35 @@
+# H4XOR
+# nxll <nxll@teknik.io>
+
+cursor="#C2FFA0"
+mouse_background="#1A3020"
+mouse_foreground="#C2FFA0"
+highlight="#1F3B27"
+border="#1A3020"
+background="#1A3020"
+foreground="#C2FFA0"
+
+#BLACK
+color0="#1D3624"
+color8="#1F3B27"
+#RED
+color1="#9BCC80"
+color9="#9CCE81"
+#GREEN
+color2="#BAFF94"
+color10="#BBFF95"
+#YELLOW
+color3="#C2FFAF"
+color11="#C3FFAF"
+#BLUE
+color4="#5F7D4E"
+color12="#607D4F"
+#MAGENTA
+color5="#C2FFA0"
+color13="#C3FFA1"
+#CYAN
+color6="#C2FFA0"
+color14="#C3FFA1"
+#WHITE
+color7="#DBFFC7"
+color15="#DCFFC8"
---
+++ .dynamic-colors/colorschemes/muted.sh
@@ -0,0 +1,35 @@
+# MUTED
+# nxll <nxll@teknik.io>
+
+cursor="#EEEEEE"
+mouse_background="#141A20"
+mouse_foreground="#EEEEEE"
+highlight="#0F151A"
+border="#141A20"
+background="#141A20"
+foreground="#EEEEEE"
+
+#BLACK
+color0="#11171C"
+color8="#0F151A"
+#RED
+color1="#AC504F"
+color9="#CC504F"
+#GREEN
+color2="#62AC4F"
+color10="#82AC4F"
+#YELLOW
+color3="#ACAC4F"
+color11="#CCAC4F"
+#BLUE
+color4="#4F87AC"
+color12="#5287AC"
+#MAGENTA
+color5="#AC4FA9"
+color13="#CC4FA9"
+#CYAN
+color6="#4FACA1"
+color14="#51ACA1"
+#WHITE
+color7="#E6E6E6"
+color15="#E8E8E8"

e105472dfeaeb398d78a716b6b17c389dc3a5546
nxll <nxll@teknik.io>
2016-12-09 17:58:28 +0000

    cleanup

--- .config/dmenu/dmenu_bind.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dmenu_run -l 10 -fn "-benis-lemon-abunai-abunai-medium-r-normal--10-110-75-75-m-50-iso10646-1" -nb "#151515" -nf "#ffffff" -sb "#aadb0f" -sf "#151515" -w "1920" -x "0" -y "0" &

865202d5e3a47dfb3b26fca55d2a8b4fd3b896ad
nxll <nxll@teknik.io>
2016-12-09 17:45:18 +0000

    cleanup

--- bin/lock/lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ICON=$HOME/bin/lock/icon.png
-TMPBG=/tmp/screen.png
-scrot /tmp/screen.png
-convert $TMPBG -blur 0x1 $TMPBG
-convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
-i3lock -u -i $TMPBG
